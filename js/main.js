b8d7f4710e616292d365439a960425edbc8018d3
Pavel <assakay92@gmail.com>
2023-10-03 00:54:06 +0300

    burger edit

--- js/main.js
+++ js/main.js
@@ -30,20 +30,61 @@ window.addEventListener('scroll', function () {
 
 
 
-// ------parralax------
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const parallaxContainer = document.querySelector('.parallax-container');
-//     const parallaxText = document.querySelector('.parallax-text');
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.querySelector(".scroll-button");
+    var scrollHeight = 740; 
+    var isFixed = false;
 
-//     window.addEventListener('scroll', function () {
-//         const scrollTop = window.scrollY;
-//         const offset = -scrollTop * 0.5;
-//         // Измените коэффициент для более или менее сильного эффекта параллакса
-//         const textColor = scrollTop > 100 ? 'black' : 'white';
-//         // Измените цвет текста в зависимости от скролла
+    window.addEventListener("scroll", function() {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-//         parallaxContainer.style.transform = `translateY(${offset}px)`;
-//         // parallaxText.style.color = textColor;å
-//     });
-// });
+        if (scrollTop >= scrollHeight && !isFixed) {
+            button.style.top = scrollHeight + "px";
+            isFixed = true;
+        } else if (scrollTop < scrollHeight && isFixed) {
+            button.style.top = "";
+            isFixed = false;
+        }
+    });
+});
+
+
+
+// -------burger------
+
+document.addEventListener("DOMContentLoaded", function() {
+    var burgerIcon = document.querySelector(".burger-icon");
+    var burgerMenu = document.querySelector(".burger-menu");
+
+    burgerIcon.addEventListener("click", function() {
+        burgerIcon.classList.toggle("active"); // Анимация иконки бургера
+        burgerMenu.classList.toggle("active"); // Открытие/закрытие меню
+
+        if (burgerMenu.classList.contains("active")) {
+            document.body.style.overflow = "hidden"; // Запрет прокрутки страницы при открытом меню
+        } else {
+            document.body.style.overflow = ""; // Разрешение прокрутки страницы при закрытом меню
+        }
+    });
+});
+
+
+
+// ==========pop_pu=======
+
+document.addEventListener("DOMContentLoaded", function() {
+    var openButton = document.getElementById("open_pop");
+    var closeButton = document.getElementById("pop_close");
+    var popUp = document.querySelector(".pop_up");
+
+    // Добавление класса "active" при нажатии на кнопку "Открыть попап"
+    openButton.addEventListener("click", function() {
+        popUp.classList.add("active");
+    });
+
+    // Удаление класса "active" при нажатии на кнопку "Закрыть попап"
+    closeButton.addEventListener("click", function() {
+        popUp.classList.remove("active");
+    });
+});
